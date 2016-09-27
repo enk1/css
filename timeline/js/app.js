05f8738f15a53546f3c7a85d00aed22cf037be05
enk1 <grzywaczewski.jakub@gmial.com>
2016-09-27 15:12:52 +0200

    timeline - cleaning structure

--- timeline/js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.wrapper').prepend('<div class="menuTrigger">Menu</div>');
-    $('.menuTrigger').on('click', function() {
-        $('.menu').slideToggle();
-    });
-});

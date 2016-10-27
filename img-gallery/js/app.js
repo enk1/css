cec7b3656f3b1e382c9cc3447cd5fd3dc67632d3
Jakub <grzywaczewski.jakub@gmail.com>
2016-10-27 21:18:05 +0200

    fancy box added and filtering images

--- img-gallery/js/app.js
+++ img-gallery/js/app.js
@@ -1,3 +1,31 @@
-$(document).ready(function(){
-    console.log('asdasd');
+$(document).ready(function() {
+    $(".fancybox").fancybox({
+        helpers: {
+            buttons: {}
+        }
+    });
+
+    $('.filter').on('click', function(){
+        if(!($(this).hasClass('active'))) {
+            $('.filter').removeClass('active');
+            $(this).addClass('active');
+            
+            var filter = $(this).data('rel');
+            
+            'all' == filter ?
+                $('.fancybox')
+                .attr('data-fancybox-group','gallery')
+                .not(':visible')
+                .fadeIn()
+                :
+                $('.fancybox')
+                .fadeOut(0)
+                .filter(function(){
+                    return $(this).data('filter') == filter;
+                })
+                .attr('data-fancybox-group',filter)
+                .fadeIn(1000);
+            
+        }
+    });
 });
